f131a4b9c903f9c0d27da5f5b5b84b4c73400afd
yk <example@example.com>
2018-02-03 07:20:31 +0900

    Add project 4 version 2

---
+++ yk/nand2tetris/04/fill/version2/Fill.asm
@@ -0,0 +1,40 @@
+// Project 04 - Fill.asm
+(CHECK_KEYBOARD)
+    @SCREEN     // pen := SCREEN
+    D=A
+    @pen
+    M=D
+    @KBD        // if KBD = 0 then goto FILL_WHITE
+    D=M
+    @FILL_WHITE
+    D;JEQ
+    @FILL_BLACK // goto FILL_BLACK
+    0;JMP
+(FILL_WHITE)
+    @pen        // Memory[pen] := 0
+    A=M
+    M=0
+    @pen        // pen := pen + 1
+    M=M+1
+    @KBD        // if pen = KBD goto CHECK_KEYBOARD
+    D=A
+    @pen
+    D=M-D
+    @CHECK_KEYBOARD
+    D;JEQ
+    @FILL_WHITE // goto FILL_WHITE
+    0;JMP
+(FILL_BLACK)
+    @pen        // Memory[pen] := -1
+    A=M
+    M=-1
+    @pen        // pen := pen + 1
+    M=M+1
+    @KBD        // if pen = KBD goto CHECK_KEYBOARD
+    D=A
+    @pen
+    D=M-D
+    @CHECK_KEYBOARD
+    D;JEQ
+    @FILL_BLACK // goto FILL_BLACK
+    0;JMP
---
+++ yk/nand2tetris/04/mult/version2/Mult.asm
@@ -0,0 +1,40 @@
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// where R0 >= 0 and R1 >= 0 and R0 * R1 < 32768.
+    @R2     // R2 := 0
+    M=0
+    @16     // count := 16
+    D=A
+    @count
+    M=D
+    @mask   // mask := 1
+    M=1
+(LOOP)
+    @mask   // if mask & R0 = 0 then goto IGNORE
+    D=M
+    @R0
+    D=D&M
+    @IGNORE
+    D;JEQ
+    @R1     // R2 += R1
+    D=M
+    @R2
+    M=D+M
+(IGNORE)
+    @mask   // mask = 2 * mask
+    D=M
+    M=D+M
+    @R1     // R1 = 2 * R1
+    D=M
+    M=D+M
+    @count  // count := count - 1
+    M=M-1
+    @count  // if count = 0 then goto END
+    D=A
+    @END
+    D;JEQ
+    @LOOP   // goto LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
